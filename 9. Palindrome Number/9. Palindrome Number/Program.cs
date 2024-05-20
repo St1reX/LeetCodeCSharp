ae7d5d91496f124ae814563b3046a1bcbc78fa29
Jakub Uryga <128072850+tymek300@users.noreply.github.com>
2024-05-20 20:03:14 +0200

    Add files via upload

---
+++ 9. Palindrome Number/9. Palindrome Number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(-121));
+        }
+
+        static public bool IsPalindrome(int x)
+        {
+            bool result = true;
+            string s1 = x.ToString();
+
+            for (int i = 0, j=s1.Length-1; i<(s1.Length/2) + s1.Length%2; i++, j--)
+            {
+                if (s1[i] != s1[j])
+                {
+                    result = false; 
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
